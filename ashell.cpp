091d5be7d80c1f03cfd2136178efbbcee2f9cdb8
Raymond Christy <Raymondc159@gmail.com>
2015-04-11 16:44:24 -0700

    ls is working, this means all built in functions are good

--- ashell.cpp
+++ ashell.cpp
@@ -23,7 +23,7 @@ int alive = 1;
 char *curWD = new char[PATH_MAX];
 char *home_path = getenv("HOME");
 vector<string> history;
-int prev = 0;
+int hist_index = 0;
 
 void ResetCanonicalMode(int fd, struct termios *savedattributes)
 {
@@ -97,6 +97,113 @@ void addToHistory(string str)
   history.push_back(str);
 }
 
+void printPermissions(char *dir_name, char *file_name)
+{
+  struct stat stats;
+  string full_path;
+  full_path += dir_name;
+  full_path += '/';
+  full_path += file_name;
+  
+  stat(full_path.c_str(), &stats);
+  
+  //directory check
+  if((stats.st_mode & S_IFMT) == S_IFDIR)
+  {
+    write(STDOUT_FILENO, "d", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  //owner permissions
+  if(stats.st_mode & S_IRUSR)
+  {
+    write(STDOUT_FILENO, "r", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  if(stats.st_mode & S_IWUSR)
+  {
+    write(STDOUT_FILENO, "w", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  if(stats.st_mode & S_IXUSR)
+  {
+    write(STDOUT_FILENO, "x", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  //group permissions
+  if(stats.st_mode & S_IRGRP)
+  {
+    write(STDOUT_FILENO, "r", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  if(stats.st_mode & S_IWGRP)
+  {
+    write(STDOUT_FILENO, "w", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  if(stats.st_mode & S_IXGRP)
+  {
+    write(STDOUT_FILENO, "x", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  //other permissions
+  if(stats.st_mode & S_IROTH)
+  {
+    write(STDOUT_FILENO, "r", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  if(stats.st_mode & S_IWOTH)
+  {
+    write(STDOUT_FILENO, "w", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  if(stats.st_mode & S_IXOTH)
+  {
+    write(STDOUT_FILENO, "x", 1);
+  }
+  else
+  {
+    write(STDOUT_FILENO, "-", 1);
+  }
+  
+  write(STDOUT_FILENO, " ", 1);
+}
+
 void parseInput(string line_in)
 {
   if(line_in.empty())
@@ -123,13 +230,68 @@ void parseInput(string line_in)
     else
     {
       char *direc = args[1];
-      chdir(direc);
+      if(chdir(direc) == -1)
+      {
+        if(errno == EACCES)
+        {
+          write(STDOUT_FILENO, "Permission denied.\n", 19);
+        }
+        else if(errno == ENOTDIR)
+        {
+          write(STDOUT_FILENO, direc, strlen(direc));
+          write(STDOUT_FILENO, " not a directory!\n", 18);
+        }
+        else
+        {
+          write(STDOUT_FILENO, "Error changing directory.\n", 26);
+        }
+      }
     }
     getcwd(curWD, PATH_MAX);
     return;
   } 
   else if(strcmp(args[0], "ls") == 0)
   {
+    char *dir_name;
+    if(args[1] == NULL)
+    {
+      dir_name = curWD;
+    }
+    else
+    {
+      dir_name = args[1];
+    }
+    
+    DIR *dir = opendir(dir_name);
+    
+    if(dir == NULL)
+    {
+      if(errno == EACCES)
+        {
+          write(STDOUT_FILENO, "Permission denied.\n", 19);
+        }
+        else if(errno == ENOTDIR)
+        {
+          write(STDOUT_FILENO, dir_name, strlen(dir_name));
+          write(STDOUT_FILENO, " not a directory!\n", 18);
+        }
+        else
+        {
+          write(STDOUT_FILENO, "Error listing directory.\n", 25);
+        }
+    }
+    else
+    {
+      struct dirent *dp;
+      while((dp = readdir(dir)) != NULL)
+      {
+        printPermissions(dir_name, dp->d_name);
+        write(STDOUT_FILENO, dp->d_name, strlen(dp->d_name));
+        write(STDOUT_FILENO, "\n", 1);
+      }
+      
+    }
+    closedir(dir);   
     return;
   }
   else if(strcmp(args[0], "pwd") == 0)
@@ -191,7 +353,7 @@ int main(int argc, char **argv)
       if(temp.length()>16)
       {
         int slash = temp.rfind('/');
-        temp = temp.substr(slash,temp.length());
+        temp = temp.substr(slash, temp.length());
         temp = "/..." + temp;
       }
       write(STDOUT_FILENO, temp.c_str(), temp.length());
@@ -203,11 +365,11 @@ int main(int argc, char **argv)
     {
       //enter pressed
       case 0x0A:
-        write(STDOUT_FILENO, "\n",2);
+        write(STDOUT_FILENO, "\n", 1);
         addToHistory(line_in);
         parseInput(line_in);
         line_in = "";
-        prev = 0;
+        hist_index = 0;
         prPrompt = 1;
         break;
       //backspace pressed
@@ -222,7 +384,7 @@ int main(int argc, char **argv)
           line_in.erase(line_in.length() - 1);
         }
         break;
-      //arrow key pressed
+      //control sequence pressed
       case 0x1B:      
         read(STDIN_FILENO, &char_in, 1);
         if(char_in != 0x5B)
@@ -234,13 +396,13 @@ int main(int argc, char **argv)
         if(char_in == 0x41)
         {
           string temp;          
-          if(history.empty() || prev + 1 > history.size())
+          if(history.empty() || hist_index + 1 > history.size())
           {
             write(STDOUT_FILENO, "\a", 1);
             break;
           }
-          prev++;
-          temp = history[history.size() - prev];
+          hist_index++;
+          temp = history[history.size() - hist_index];
           for(int i = 0; i < line_in.size(); i++)
           {
             write(STDOUT_FILENO, "\b \b", 3);
@@ -253,19 +415,19 @@ int main(int argc, char **argv)
         else if(char_in == 0x42)
         {
           string temp;
-          if(prev == 0)
+          if(hist_index == 0)
           {
             write(STDOUT_FILENO, "\a", 1);
             break;
           }
-          prev--;
-          if(prev == 0)
+          hist_index--;
+          if(hist_index == 0)
           {
             temp = "";
           }
           else
           {
-            temp = history[history.size() - prev];
+            temp = history[history.size() - hist_index];
           }
           for(int i = 0; i < line_in.size(); i++)
           {
